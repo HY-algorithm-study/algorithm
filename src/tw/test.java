e8528fd46d318bd96042880324456e14eaab1e0e
김홍표 <rla7538@naver.com>
2018-10-25 22:56:13 +0900

    set taewan env

---
+++ src/tw/test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by hong2 on 2018. 10. 25.
+ * Time : PM 10:55
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}

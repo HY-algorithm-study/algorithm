206e70efe5a6c77c4fe007fd30d2a0d7512ddf6b
Yomni <yearsheep@gmail.com>
2018-11-01 16:08:09 +0900

    template file push

---
+++ src/ym/programmers/Network.js
@@ -0,0 +1,18 @@
+function solution(n, computers) {
+    let answer = 0;
+    // 1. 각 node를 나타내는 map 혹은 배열 선언(방문 완료)
+    let queue = new Array(n).fill(false);
+
+    while(node.contains(false)) {
+        bfs(computers,queue);
+        // 2. 주어진 computers 배열을 bfs 혹은 dfs 탐색
+        //      2-1. 방문 완료 시 nodes(array) 갱신
+        //      2-2. 방문할 수 없는 node 발견 시 answer 증가
+    }
+
+    return answer;
+}
+
+function bfs(nodes, queue){
+
+}

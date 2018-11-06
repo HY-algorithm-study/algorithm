636608af1837047a25bf833f3234d2b720818105
hong-pyo <ghdvy010@gmail.com>
2018-11-07 01:55:02 +0900

    [PRG-43163] change words not resolve_11/7

--- src/hp/programmers/ChangeWord_43163.java
+++ src/hp/programmers/ChangeWord_43163.java
@@ -1,10 +1,7 @@
 package programmers;
 
-import com.google.common.collect.Maps;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by hong2 on 03/11/2018
@@ -12,6 +9,9 @@ import java.util.Map;
  */
 
 public class ChangeWord_43163 {
+    Map<String, List<String>> wordSet;
+    String target = "cog";
+
     public static void main(String[] args) {
         String begin = "hit";
         String target = "cog";
@@ -19,20 +19,62 @@ public class ChangeWord_43163 {
         String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
 
         ChangeWord_43163 changeWord_43163 = new ChangeWord_43163();
-        changeWord_43163.solution(begin, target, words);
+        System.out.println(changeWord_43163.solution(begin, target, words));
     }
 
     public int solution(String begin, String target, String[] words) {
+        int answer = 0;
         List<String> strings = Arrays.asList(words);
-        Map<String, List<String>> stringListMap = Maps.newHashMap();
         if (!strings.contains(target)) {
             return 0;
         }
-
-        int answer = 0;
+        wordSet = makeWordSet(begin, words);
+        List<String> history = new ArrayList<>();
+        String t = begin;
+        countAvailableLoad(begin, t, answer);
         return answer;
     }
 
+    private int countAvailableLoad(String begin, String history, int count) {
+
+        List<String> availAble = wordSet.get(begin);
+
+        for (String word : availAble) {
+            if (history.contains(target)) {
+                return count;
+            }
+            if (history.contains(word)) {
+                continue;
+            } else {
+                count++;
+                history += word;
+                countAvailableLoad(word, history, count);
+            }
+        }
+
+        return count;
+    }
+
+    // 각 단어마다 갈 수 있는 리스트들을 만들어줌.
+    private Map<String, List<String>> makeWordSet(String begin, String[] words) {
+        Map<String, List<String>> stringListMap = new HashMap<>();
+
+        stringListMap.put(begin, findWordLoad(begin, words));
+
+        for(String word : words) {
+            stringListMap.put(word, findWordLoad(word, words));
+        }
+
+        return stringListMap;
+    }
+
+
+    // 갈 수 있는 길의 리스트들을 뽑아줌
+    private List<String> findWordLoad(String begin, String[] words) {
+        return Arrays.asList(words).stream().filter(word -> checkSumString(begin, word)).collect(Collectors.toList());
+    }
+
+    // 갈 수 있는 길을 찾는다.
     private boolean checkSumString(String a, String b) {
         int result = 0;
         if (a.length() != b.length()) {

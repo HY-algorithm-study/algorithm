bcf20b388b2d117e1ce697cd97bd134e0c3706ae
hong-pyo <ghdvy010@gmail.com>
2018-11-08 01:07:20 +0900

    [PRG-43163] change words solve

---
+++ src/hp/programmers/ChangeWord_43163.java
@@ -0,0 +1,100 @@
+package programmers;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Created by hong2 on 03/11/2018
+ * Time : 5:57 PM
+ */
+
+public class ChangeWord_43163 {
+    Map<String, List<String>> wordSet;
+
+    public static void main(String[] args) {
+        String begin = "hit";
+        String target = "cog";
+
+        String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
+        //String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
+
+        ChangeWord_43163 changeWord_43163 = new ChangeWord_43163();
+        System.out.println(changeWord_43163.solution(begin, target, words));
+    }
+
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        List<String> strings = Arrays.asList(words);
+        if (!strings.contains(target)) {
+            return 0;
+        }
+        wordSet = makeWordSet(begin, words);
+
+        Set<String> candidate = new HashSet<>();
+        Map<String, List<String>> available = new HashMap<>();
+
+        candidate.add(begin);
+        available.put(begin, wordSet.get(begin));
+        answer = findOptimizeRoute(candidate, available, target, answer);
+
+        return answer+1;
+    }
+
+   private int findOptimizeRoute(Set<String> candidate, Map<String, List<String>> available, String target, int count) {
+
+        Set<String> temp = new HashSet<>();
+        Map<String, List<String>> availMap = new HashMap<>();
+        for (String word : candidate) {
+            if (available.get(word).contains(target)) {
+                return count;
+            }
+            temp.addAll(available.get(word));
+        }
+
+        temp.forEach(word -> {
+            availMap.put(word, wordSet.get(word));
+        });
+
+       return findOptimizeRoute(temp, availMap, target, ++count);
+   }
+
+    // 각 단어마다 갈 수 있는 리스트들을 만들어줌.
+    private Map<String, List<String>> makeWordSet(String begin, String[] words) {
+        Map<String, List<String>> stringListMap = new HashMap<>();
+
+        stringListMap.put(begin, findWordLoad(begin, words));
+
+        Arrays.asList(words).forEach(word -> {
+            stringListMap.put(word, findWordLoad(word, words));
+        });
+
+        return stringListMap;
+    }
+
+
+    // 갈 수 있는 길의 리스트들을 뽑아줌
+    private List<String> findWordLoad(String begin, String[] words) {
+        return Arrays.asList(words).stream().filter(word -> checkSumString(begin, word)).collect(Collectors.toList());
+    }
+
+    // 갈 수 있는 길을 찾는다.
+    private boolean checkSumString(String a, String b) {
+        int result = 0;
+
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        for(int i = 0; i<a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                result++;
+            }
+        }
+
+        if (result == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/hp/programmers/TargetNumber_43165.java
@@ -0,0 +1,58 @@
+package programmers;
+
+/**
+ * Created by hong2 on 01/11/2018
+ * Time : 8:44 PM
+ */
+
+public class TargetNumber_43165 {
+    int answer = 0;
+    public static void main(String[] args) {
+        TargetNumber_43165 targetNumber_43165 = new TargetNumber_43165();
+        int [] numbers = {1,1};
+        int target = 2;
+
+        System.out.println(targetNumber_43165.solution(numbers, target));
+    }
+    public int solution(int[] numbers, int target) {
+
+        return findTarget(numbers, target, 0);
+    }
+
+    private int findTarget(int[] numbers, int target, int index) {
+        if (index == numbers.length) {
+            return answer;
+        }
+
+        findTarget(numbers, target, index+1);
+        System.out.println(numbers.toString());
+        if (findSum(numbers, target)) {
+            answer++;
+        }
+        numbers[index] *= -1;
+        findTarget(numbers, target, index+1);
+
+        return answer;
+    }
+
+    private void print(int[] num) {
+        for (int i =0; i<num.length; i++) {
+            System.out.print(num[i]);
+        }
+
+        System.out.println();
+    }
+
+    private boolean findSum(int[] numbers, int target) {
+        int sum = 0;
+        for (int i=0; i<numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        if (sum == target) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

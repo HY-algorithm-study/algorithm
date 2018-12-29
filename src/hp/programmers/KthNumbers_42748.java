cb63aa0238b598fedfe148dde471f88b88a74634
hong-pyo <ghdvy010@gmail.com>
2018-12-29 17:21:40 +0900

    [KthNumbers_42748] test push

---
+++ src/hp/programmers/KthNumbers_42748.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.Arrays;
+
+/**
+ * Created by hong2 on 29/12/2018
+ * Time : 5:15 PM
+ */
+
+public class KthNumbers_42748 {
+    public static void main(String[] args) {
+        KthNumbers_42748 kthNumbers_42748 = new KthNumbers_42748();
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] command = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        System.out.println(Arrays.toString(kthNumbers_42748.solution(array, command)));
+    }
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+
+        return answer;
+    }
+}
